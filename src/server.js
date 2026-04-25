e707fe0d1271f533370a8a345c5d51209ed2af6a
mauliknananiya144 <mauliknananiya@gmail.com>
2026-04-25 11:28:24 +0530

    change the db url

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
+const DATABASE_URL = process.env.DATABASE_URL;
 app.get("/", (req, res) => {
   res.send("Api is Running");
 });
-let DATABASE_URL = "mongodb://127.0.0.1:27017/maulik-portfolio";
+
 mongoose
-  .connect(DATABASE_URL, {})
+  .connect(DATABASE_URL)
   .then(() => {
     console.log("✅ MongoDB Connected Successfully");
   })
